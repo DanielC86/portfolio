1a74d0ce40e6210f61aa328da48df75c3ca03e74
Daniel <daniel.czajka@gmail.com>
2020-03-04 14:50:00 +0100

    adding CRUD - GET/POST/PUT/DELETE functionalities to employee table

--- src/main/java/com/daniel/czajka/rest/EmployeeRestController.java
+++ src/main/java/com/daniel/czajka/rest/EmployeeRestController.java
@@ -1,12 +1,9 @@
 package com.daniel.czajka.rest;
 
-import com.daniel.czajka.dao.EmployeeDAO;
 import com.daniel.czajka.entity.Employee;
 import com.daniel.czajka.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,6 +24,51 @@ public class EmployeeRestController {
     public List<Employee> findAll() {
         return employeeService.findAll();
     }
+
+    //add mapping for GET /employees/employeeId
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee == null){
+            throw new RuntimeException("Employee id not found: " + employeeId);
+        }
+        return theEmployee;
+    }
+
+    //add mapping for POST /employees - add new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee){
+
+        theEmployee.setId(0);
+
+        employeeService.save(theEmployee);
+
+        return theEmployee;
+    }
+
+    //add mapping for PUT /employees - update existing employee
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee){
+
+        employeeService.save(theEmployee);
+
+        return theEmployee;
+    }
+
+    //add mapping for DELETE /employees/{employeeId} - deleting existing employee by id
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId){
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        //throw exception if null
+        if(theEmployee == null){
+            throw new RuntimeException("Cannot find employee with id of: " + employeeId);
+        }
+        employeeService.deleteById(employeeId);
+        return "Deleted employee with id of: " + employeeId;
+    }
+
 
 
 
